351a32d10e84e14808767f4fc19d0aa35be62125
GiuliaCiaramella <giuliaciaramella@live.it>
2021-05-10 15:26:00 +0200

    updated

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from utils import Net
+import numpy as np
+import matplotlib.pyplot as plt
 
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def imshow(img):
+    img = img/2 +0.5 # to unormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg,(1,2,0)))
+    # plt.show()
 
 transform = transforms.Compose([transforms.ToTensor(),
                                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
@@ -12,46 +24,18 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size, shuffle = True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
+
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-import numpy as np
-import matplotlib.pyplot as plt
-def imshow(img):
-    img = img/2 +0.5 # to unormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg,(1,2,0)))
-    # plt.show()
+
 
 
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
-import torch.nn as nn
-import torch.nn.functional as F
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3,6,5)
-        self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(6,16,5)
-        self.fc1 = nn.Linear(16*5*5,120)
-        self.fc2 = nn.Linear(120,84)
-        self.fc3 = nn.Linear(84,10)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 net = Net()
 
@@ -85,44 +69,4 @@ print('Finished Training')
 
 PATH = './cifar_net.pth'
 torch.save(net.state_dict(), PATH)
-net = Net()
-net.load_state_dict(torch.load(PATH))
-outputs = net(images)
-
-
-
-_, predicted = torch.max(outputs, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(4):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
-
-
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (
-        classes[i], 100 * class_correct[i] / class_total[i]))
 
